9cbdcc5923767b9b749d72ab91a7c9fce333ebf8
Molebogeng Selahle <business.molebogeng@gmail.com>
2026-06-06 15:42:05 +0200

    1st commit

---
+++ KoKoApp/src/main/java/co/za/KoKoApp/KoKoAppApplication.java
@@ -0,0 +1,13 @@
+package co.za.KoKoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoKoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoKoAppApplication.class, args);
+	}
+
+}
---
+++ KoKoApp/src/test/java/co/za/KoKoApp/KoKoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.KoKoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoKoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
